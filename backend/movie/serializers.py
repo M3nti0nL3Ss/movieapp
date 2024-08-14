3384301d5b2abad43f7ba2f4b94d37af1c1a16cd
M3nti0nl3ss <ayoub-sifo@live.fr>
2024-08-14 13:39:11 +0000

    Adding Serializers, testing Movie serializer

---
+++ backend/movie/serializers.py
@@ -0,0 +1,39 @@
+"""
+Serializers for movies
+"""
+
+from rest_framework import serializers
+
+from core.models import Movie, Actor, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    """Serializer for movie view."""
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'description']
+        read_only_fields = ['id']
+
+
+class MovieDetailSerializer(MovieSerializer):
+    """Serializer for movie detail view."""
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta(MovieSerializer.Meta):
+        fields = MovieSerializer.Meta.fields + ['reviews', 'actors']
+        depth = 1
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    """Serializer for actors."""
+
+    class Meta:
+        model = Actor
+        fields = ['id', 'first_name', 'last_name']
+        read_only_fields = ['id']
--- backend/movie/views.py
+++ backend/movie/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+"""
+Views for movies
+"""
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from movie import serializers
+from core.models import Movie
 
-# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs."""
+    serializer_class = serializers.MovieDetailSerializer
+    queryset = Movie.objects.all()
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        """Retrieve the movie."""
+        return self.queryset.order_by('-id')
+
+    def get_serializer_class(self):
+        """Return serializer class."""
+        if self.action == 'list':
+            return serializers.MovieSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save()
