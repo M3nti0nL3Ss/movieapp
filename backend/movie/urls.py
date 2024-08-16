762597dea21a092e6518b1535f617c544c0764d3
ayoub-sifo@live.fr <ayoub-sifo@live.fr>
2024-08-16 19:03:38 +0100

    Adding drf-nested-routers

--- backend/movie/urls.py
+++ backend/movie/urls.py
@@ -7,17 +7,19 @@ from rest_framework.routers import DefaultRouter
 
 from movie import views
 
-router = DefaultRouter()
-router.register('movies', views.MovieViewSet)
-router.register('actors', views.ActorViewSet)
+from rest_framework_nested import routers
 
-movie_router = DefaultRouter()
-movie_router.register(r'reviews', views.ReviewViewSet,
-                      basename='movie-reviews')
+router = routers.DefaultRouter()
+router.register(r'movies', views.MovieViewSet)
+router.register(r'actors', views.ActorViewSet)
+
+# Création d'un router imbriqué pour les reviews
+movies_router = routers.NestedDefaultRouter(router, r'movies', lookup='movie')
+movies_router.register(r'reviews', views.ReviewViewSet, basename='movie-reviews')
 
 app_name = 'movie'
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('movies/<int:movie_pk>/', include(movie_router.urls)),
+    path('', include(movies_router.urls)),
 ]
